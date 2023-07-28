f305f08da5edf60dad93d254aaeb54afdc141d55
diglowc <81141945+diglowc@users.noreply.github.com>
2023-07-28 13:35:25 +0900

    feat: create CommunityCategoryButton.js
    Create CommunityCategoryButton.js.

---
+++ src/components/CommunityCategoryButton.js
@@ -0,0 +1,43 @@
+import { Pressable, StyleSheet } from "react-native";
+import palette from "../styles/ColorPalette";
+import Text from "./DefaultText";
+
+const CommunityCategoryButton = ({ title, onPress }) => {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        styles.button,
+        {
+          backgroundColor: "#ffffff",
+        },
+        pressed && {
+          backgroundColor: "#7DADFF",
+        },
+      ]}
+      onPressOut={onPress}
+    >
+      <Text style={styles.title}>{title}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 40,
+    borderWidth: 0.5,
+    borderColor: palette.gray2,
+    paddingHorizontal: 19,
+    height: 34,
+    marginRight: 6,
+    // 카테고리 아이템 줄 나뉘었을 때 간격을 위해 marginBottom 설정
+    marginBottom: 12,
+  },
+  title: {
+    color: palette.gray4,
+    fontSize: 14,
+  },
+});
+
+export default CommunityCategoryButton;
