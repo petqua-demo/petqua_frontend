c39c7fe1d2e94613d6e6ff6b95ae7e80cc7450f1
diglowc <81141945+diglowc@users.noreply.github.com>
2023-09-06 16:24:10 +0900

    feat: create CommunityCategoryImageIcon.js
    Create CommunityCategoryImageIcon.js.

---
+++ src/components/CommunityCategoryImageIcon.js
@@ -0,0 +1,31 @@
+import { Pressable, StyleSheet, Image } from "react-native";
+import palette from "../styles/ColorPalette";
+import Text from "./DefaultText";
+
+const CommunityCategoryImageIcon = ({ onPress, imageSrc, title }) => {
+  return (
+    <Pressable onPress={onPress} style={styles.container}>
+      <Image source={imageSrc} style={styles.iconImage}></Image>
+      <Text style={styles.title}>{title}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 8,
+  },
+  title: {
+    color: palette.gray4,
+    fontSize: 12,
+    marginTop: 12,
+  },
+});
+
+export default CommunityCategoryImageIcon;
