b9a9daa87eebaa87046c89a671ad248d2b6d1f06
diglowc <81141945+diglowc@users.noreply.github.com>
2023-10-16 20:42:25 +0900

    feat: create CommunityCategoryList.js
    Create CommunityCategoryList.js.

---
+++ src/components/CommunityCategoryList.js
@@ -0,0 +1,33 @@
+import { FlatList } from "react-native";
+import CommunityCategoryImageIcon from "./CommunityCategoryImageIcon";
+
+const CategoryItem = ({ onPress, imageSrc, title }) => (
+  <CommunityCategoryImageIcon
+    onPress={onPress}
+    imageSrc={imageSrc}
+    title={title}
+  />
+);
+
+const numColumns = 4;
+
+const CommunityCategoryList = ({ CommunityCategories, onPress }) => {
+  return (
+    <FlatList
+      data={CommunityCategories}
+      renderItem={({ item }) => (
+        <CategoryItem
+          onPress={onPress}
+          imageSrc={item.imageSrc}
+          title={item.title}
+        />
+      )}
+      showsVerticalScrollIndicator={false}
+      numColumns={numColumns}
+      scrollEnabled={false}
+      columnWrapperStyle={{ justifyContent: "space-between" }}
+    />
+  );
+};
+
+export default CommunityCategoryList;
