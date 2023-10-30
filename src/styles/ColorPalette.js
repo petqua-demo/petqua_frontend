3546c140e4f8293da7bb16dcb8bacb761877db85
diglowc <81141945+diglowc@users.noreply.github.com>
2023-10-30 15:58:29 +0900

    chore: update colors in ColorPalette
    Update colors in ColorPalette.

--- src/styles/ColorPalette.js
+++ src/styles/ColorPalette.js
@@ -1,14 +1,18 @@
 const palette = {
-  mainColor: "#004BCA",
-  navy: "#012F7D",
-  lightGray: "#D9D9D9",
-  gray1: "#F4F4F4",
-  gray2: "#B9BDC5",
-  body1: "#999999",
-  gray3: "#777777",
-  gray4: "#333333",
+  mainBlue: "#004BCA",
+  skyBlue: "#69A1FF",
+  mainGray: "#B9BDC5",
+  gray2: "#4D4D4D",
+  gray3: "#F1F1F1",
+  mainDark: "#333333",
   black: "#0B0D0F",
   warning: "#E72A00",
+
+  // navy: "#012F7D",
+  // lightGray: "#D9D9D9",
+  // gray1: "#F4F4F4",
+  // body1: "#999999",
+  // gray3: "#777777",
 };
 
 export default palette;
