04cfe9ce340fe5476935d75bd1afddaed4f321ac
diglowc <81141945+diglowc@users.noreply.github.com>
2023-11-06 14:31:24 +0900

    design: change details of CommunityCategoryRect
    Change details of CommunityCategoryRect.

--- src/components/CommunityCategoryRect.js
+++ src/components/CommunityCategoryRect.js
@@ -1,27 +1,28 @@
 import { Pressable, StyleSheet } from "react-native";
 import palette from "../styles/ColorPalette";
-import Text from "./DefaultText";
+import MediumText from "./MediumText";
 
 const CommunityCategoryRect = ({ title }) => {
   return (
     <Pressable style={styles.button}>
-      <Text style={styles.title}>{title}</Text>
+      <MediumText style={styles.title}>{title}</MediumText>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
+    alignSelf: "baseline",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 5,
-    paddingHorizontal: 8,
-    height: 18,
-    backgroundColor: "#7DADFF",
+    paddingHorizontal: 7,
+    paddingVertical: 4,
+    backgroundColor: palette.skyBlue,
   },
   title: {
     color: "#ffffff",
-    fontSize: 10,
+    fontSize: 14,
   },
 });
 
