4bf933d4952ff49fc883a4d4f929f1299fc3eeea
bwc.sean <seungwon0921@github.com>
2023-11-24 23:23:33 +0900

    feature:웹뷰 연결 수정

--- src/screens/selling/SellingListPerCategory.tsx
+++ src/screens/selling/SellingListPerCategory.tsx
@@ -1,17 +1,30 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  webbiew: {
+    flex: 1,
+    width: "100%",
+    height: 300,
+  },
+});
 
 const SellingListPerCategory = () => {
   return (
-    <WebView
-      source={{ uri: "https://petqua.github.io/petqua_frontend_webview/" }}
-      //   originWhitelist={[
-      //     "https://*",
-      //     "http://*",
-      //     "file://*",
-      //     "sms://*",
-      //     "intent://*",
-      //   ]}
-    />
+    <SafeAreaView style={styles.container}>
+      <WebView
+        style={styles.webbiew}
+        source={{
+          uri: "https://petqua.github.io/petqua_frontend_webview/",
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
