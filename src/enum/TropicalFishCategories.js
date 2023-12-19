34dc19a18797efb123f5c6368a84615d92e1cefe
diglowc <81141945+diglowc@users.noreply.github.com>
2023-12-20 00:57:48 +0900

    feat: 열대어 카테고리 enum 파일 생성

---
+++ src/enum/TropicalFishCategories.js
@@ -0,0 +1,13 @@
+const TropicalFishCategories = {
+  ricefish: "난태생/송사리과",
+  characin: "카라신과",
+  carp: "잉어과",
+  brackish: "기수어과",
+  big: "대형어",
+  anabas: "아나바스과",
+  catfish: "메기과",
+  cichlid: "시클리드과",
+  otherTropical: "기타과",
+};
+
+export default TropicalFishCategories;
